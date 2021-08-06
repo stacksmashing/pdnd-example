5235f091098a536a2f1fadf01e0507ea15667c03
Thomas Roth <thomas.roth@leveldown.de>
2021-08-06 16:16:47 +0200

    Init

---
+++ hello.h
@@ -0,0 +1,4 @@
+
+#include "pdnd/pdnd_display.h"
+
+extern pdnd_screen hello;
---
+++ main.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <string.h>
+
+#include "tusb.h"
+#include "pdnd/pdnd.h"
+#include "pdnd/pdnd_display.h"
+
+#include "hello.h"
+
+void display_inputs() {
+    cls(false);
+    pprintf("Hello World\nInputs:");
+    char inputs[32] = "";
+
+    // Iterate all inputs and display them.
+    for(int i=0; i < 8; i++) {
+        if(pdnd_in(i)) {
+            strcat(inputs, "1 ");
+        } else {
+            strcat(inputs, "0 ");
+        }
+    }
+    pprintfxy(0, 40, inputs);
+}
+
+int main() {
+    // Initialize USB-UART as STDIO
+    stdio_init_all();
+
+    // Initialize pico-debug-n-dump lib
+    pdnd_initialize();
+
+    // Initialize display
+    pdnd_display_initialize();
+    
+    // At this point in time, the buffers are off:
+    // Both outputs and inputs are in high-impedance mode.
+
+
+    // hello comes from hello.h and was created using make_screen
+    // (part of pdnd-lib)
+    pdnd_display_screen(pdnd_global_display, &hello);
+    sleep_ms(3000);
+
+    // Next, we enable the buffers.
+    pdnd_enable_buffers(1);
+
+
+    while(1) {
+        display_inputs();
+        sleep_ms(1000);
+    }
+    
+    return 0;
+}
